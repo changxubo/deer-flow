e9990b1321df3891cdece8f25d0d3f2cdb0f1c23
changxubo <bob-chang@outlook.com>
2026-03-04 16:51:18 +0000

    fix: Adjust BotMessageSquare visibility and update Wukong text size in WorkspaceHeader component

--- frontend/src/components/workspace/workspace-header.tsx
+++ frontend/src/components/workspace/workspace-header.tsx
@@ -41,13 +41,13 @@ export function WorkspaceHeader({ className }: { className?: string }) {
                 href="/"
                 className="text-primary ml-2 flex items-center gap-2 "
               >
-                <BotMessageSquare  size={20} />
-                <span className="text-md font-normal">Wukong</span>
+                <BotMessageSquare  size={20} className="hidden"/>
+                <span className="text-lg font-normal">Wukong</span>
               </Link>
             ) : (
               <div className="text-primary ml-2 flex cursor-default items-center gap-2 ">
-                <BotMessageSquare  size={20} />
-                <span className="text-md font-normal">Wukong</span>
+                <BotMessageSquare  size={20} className="hidden"/>
+                <span className="text-lg font-normal">Wukong</span>
               </div>
             )}
             <SidebarTrigger />
